468ce6b5554b66e19e239677055e15e5d75ac690
arnottferels <64685389+arnottferels@users.noreply.github.com>
2024-06-28 22:49:10 +0700

    Updates

---
+++ component---src-pages-license-js-6cf7ca15d9fe67a8a4d8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_site=self.webpackChunkmy_site||[]).push([[2706],{3526:function(e,t,n){n.r(t),n.d(t,{Head:function(){return r}});var s=n(7294),a=n(9126),c=n(8894);t.default=()=>s.createElement(a.BR,{pageTitle:"Terms",extraClass:"sans-pretty"},s.createElement("div",{style:{height:"fit-content"}},s.createElement("p",{className:"g-mono g-text-center"},"Coming soon.")));const r=()=>s.createElement(c.Ct,{title:"License"})}}]);
+//# sourceMappingURL=component---src-pages-license-js-6cf7ca15d9fe67a8a4d8.js.map
