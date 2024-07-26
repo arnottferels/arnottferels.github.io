eda61c501bd12cc4ecd688312988c13956e950a3
arnottferels <64685389+arnottferels@users.noreply.github.com>
2024-07-26 15:48:48 +0700

    Updates

--- _astro/hoisted.CP0xyEQo.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.DhQr9aP7.js";import"./index.CTbVEFV3.js";const e={init:function(){new PagefindUI({element:"#searchContainer",debounceTimeoutMs:500,resetStyles:!1,showEmptyFilters:!1,excerptLength:10,showImages:!0,showSubResults:!1,autofocus:!0})}};e.init();document.addEventListener("astro:after-swap",e.init);
