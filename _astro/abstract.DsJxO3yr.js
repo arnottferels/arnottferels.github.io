3098c83d4ef9fed0449a9fb1fd2c706598211e72
arnottferels <64685389+arnottferels@users.noreply.github.com>
2024-07-27 20:34:04 +0700

    Updates

--- _astro/abstract.DsJxO3yr.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["_astro/abstract.CKbKm6ZL.js","_astro/_astro_assets.BwkKiKDq.js","_astro/hoisted.DW6I3GbW.js","_astro/index.CTbVEFV3.js","_astro/astro/assets-service.vIrN2Q6E.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as t}from"./astro/assets-service.vIrN2Q6E.js";async function o(){return t(()=>import("./abstract.CKbKm6ZL.js"),__vite__mapDeps([0,1,2,3,4]))}const e="@@ASTRO-LINKS@@",c="@@ASTRO-STYLES@@",r="@@ASTRO-SCRIPTS@@",l={__astroPropagation:!0,getMod:o,collectedLinks:e,collectedStyles:c,collectedScripts:r};export{l as default};
---
+++ _astro/hoisted.D19F7n69.js
@@ -0,0 +1,1 @@
+import"./hoisted.Rnw5QASa.js";import"./index.CTbVEFV3.js";import"./astro/assets-service.vIrN2Q6E.js";const e={init:function(){new PagefindUI({element:"#searchContainer",debounceTimeoutMs:500,resetStyles:!1,showEmptyFilters:!1,excerptLength:10,showImages:!0,showSubResults:!1,autofocus:!0})}};e.init();document.addEventListener("astro:after-swap",e.init);
