0a41766a24fd4b4040622f98a542237bfb0d9448
arnottferels <64685389+arnottferels@users.noreply.github.com>
2024-07-28 00:56:59 +0700

    Updates

---
+++ _astro/post-3.DT7ZnL4b.js
@@ -0,0 +1,29 @@
+import{c as t,r as o,m as s,u as a}from"./hoisted.DYioZ8NS.js";import"./index.CTbVEFV3.js";import"./astro/assets-service.vIrN2Q6E.js";const n=`<p>Welcome to my <em>new blog</em> about learning Astro! Here, I will share my learning journey as I build a new website.</p>
+<h2 id="what-ive-accomplished" class="anchor-heading">What I’ve accomplished<a class="anchor-link" data-pagefind-ignore="" href="#what-ive-accomplished">#</a></h2>
+<ol>
+<li>
+<p><strong>Installing Astro</strong>: First, I created a new Astro project and set up my online accounts.</p>
+</li>
+<li>
+<p><strong>Making Pages</strong>: I then learned how to make pages by creating new <code>.astro</code> files and placing them in the <code>src/pages/</code> folder.</p>
+</li>
+<li>
+<p><strong>Making Blog Posts</strong>: This is my first blog post! I now have Astro pages and Markdown posts!</p>
+</li>
+</ol>
+<h2 id="whats-next" class="anchor-heading">What’s next<a class="anchor-link" data-pagefind-ignore="" href="#whats-next">#</a></h2>
+<p>I will finish the Astro tutorial, and then keep adding more posts. Watch this space for more to come.</p>`,r={env_prod_ignore:!0,post_type:"blog",title:"My First Blog Post",pub_date:"2022-07-01",description:"This is the first post of my new Astro blog.",authors:["Astro Learner"],keywords:["astro","blogging","learning in public"]},i="D:/izvw/github/astro/mad-jet/src/content/blog/post-3.md",l=void 0;function w(){return`
+Welcome to my _new blog_ about learning Astro! Here, I will share my learning journey as I build a new website.
+
+## What I've accomplished
+
+1. **Installing Astro**: First, I created a new Astro project and set up my online accounts.
+
+2. **Making Pages**: I then learned how to make pages by creating new \`.astro\` files and placing them in the \`src/pages/\` folder.
+
+3. **Making Blog Posts**: This is my first blog post! I now have Astro pages and Markdown posts!
+
+## What's next
+
+I will finish the Astro tutorial, and then keep adding more posts. Watch this space for more to come.
+`}function f(){return n}function b(){return[{depth:2,slug:"what-ive-accomplished",text:"What I’ve accomplished#"},{depth:2,slug:"whats-next",text:"What’s next#"}]}const y=t((c,h,p)=>{const{layout:d,...e}=r;return e.file=i,e.url=l,o`${s()}${a(n)}`});export{y as Content,f as compiledContent,y as default,i as file,r as frontmatter,b as getHeadings,w as rawContent,l as url};
