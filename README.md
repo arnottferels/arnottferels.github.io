9f19727e2cb23f198511420dc443891991237f24
arnottferels <64685389+arnottferels@users.noreply.github.com>
2024-07-28 10:52:31 +0700

    Updates

---
+++ README.md
@@ -0,0 +1,42 @@
+# Personal Website
+
+<p align="center">
+  <a href="https://arnottferels.github.io">
+    <img alt="Arnott Ferels" src="https://lh3.googleusercontent.com/a/ACg8ocLO79ybAYLKb9QKF1d5m6pf7ITmZSkGdsXk2rpcz-x4GLKwpu6kSQ=s288-c-no" width="60" />
+  </a>
+</p>
+
+Welcome to [my personal website](https://arnottferels.github.io), currently under development. This site is designed to showcase my work, ideas, and blog. Feel free to explore and learn more about what I do.
+
+## Features
+
+- [Projects](https://arnottferels.github.io/projects): A collection of my professional work and projects.
+- [Blog](https://arnottferels.github.io/blog): My insights, experiences, and reflections shared through blog posts.
+- [About](https://arnottferels.github.io/about): Information about my background, skills, and interests.
+
+## Development Status
+
+The website is a _work in progress_. Here are some features currently being developed:
+
+- Enhanced portfolio section with detailed [projects](https://arnottferels.github.io/projects) descriptions.
+- Improved all content layouts (e.g., [projects](https://arnottferels.github.io/projects), [models](https://arnottferels.github.io/models), and [blog](https://arnottferels.github.io/blog)) with a bunch of tags like [keywords](https://arnottferels.github.io/keywords), [softwares](https://arnottferels.github.io/softwares), etc.
+- Interactive elements for a more engaging user experience, e.g., I use [ViewTransition](https://docs.astro.build/en/guides/view-transitions/) by Astro.
+
+## Technologies Used
+
+- **HTML5, CSS3, & Vanilla JavaScript:** For structuring, styling, and adding interactivity to the website. I also use [SASS](https://sass-lang.com/) (SCSS) to make the CSS more maintainable and efficient. I minimize the use of JavaScript to ensure the website is efficient and performant.
+- **Static Site Generator:** I use [Astro](https://astro.build/) for easy content management and deployment.
+- **Hosting:** I use [GitHub Pages](https://pages.github.com/) for seamless deployment and hosting.
+- **Search:** I use [Pagefind](https://pagefind.app/). You can check the implemented search functionality [here](https://arnottferels.github.io/search).
+
+## Access, Feedback, and Future Plans
+
+You can visit the website [here](https://arnottferels.github.io).
+
+Your feedback is valuable as I continue to improve the website. If you have any suggestions or encounter any issues, please contact me [here](https://arnottferels.github.io/contact).
+
+Future plans include:
+- Integrating a contact form for easier communication.
+- Optimizing for better performance and accessibility.
+
+Thank you for visiting my website.
---
+++ _astro/ktmb-malaysia.a4Nhx3EM.js
@@ -0,0 +1,27 @@
+import{c as i,r as t,m as n,u as r}from"./hoisted.C8MM94eF.js";import"./index.CTbVEFV3.js";import"./astro/assets-service.vIrN2Q6E.js";const e=`<h1 id="introduction" class="anchor-heading">Introduction<a class="anchor-link" data-pagefind-ignore="" href="#introduction">#</a></h1>
+<p>Greetings, world! I’m excited to share my first blog post with you.</p>
+<h1 id="about-the-featured-image" class="anchor-heading">About the Featured Image<a class="anchor-link" data-pagefind-ignore="" href="#about-the-featured-image">#</a></h1>
+<p>The image above captures an intriguing perspective of an old building, taken on January 31, 2019, using my Fujifilm XT20 camera. It showcases the Railway Administration Building in Kuala Lumpur, also known as Keretapi Tanah Melayu Berhad (KTMB). To learn more about this building, you can visit its <a href="https://www.ktmb.com.my/">official website</a>.</p>
+<p>For additional details about the Railway Administration Building, refer to the References section below.</p>
+<h2 id="references" class="anchor-heading">References<a class="anchor-link" data-pagefind-ignore="" href="#references">#</a></h2>
+<ol>
+<li>Railway Administration Building in Kuala Lumpur, <a href="https://malaysiatrains.com/railway-administration-building-in-kuala-lumpur/">Malaysia Trains</a></li>
+<li>Gedung Ibu Pejabat Stasiun Kereta Api Tanah Melayu, <a href="https://id.wikipedia.org/w/index.php?title=Gedung_Ibu_Pejabat_Stasiun_Kereta_Api_Tanah_Melayu&#x26;oldid=23924685">Wikipedia</a></li>
+<li>KTMB Keretapi Tanah Melayu Berhad, <a href="https://maps.app.goo.gl/86AnQT5uwqYoGSux8">Google Maps</a></li>
+</ol>`,o={post_type:"blog",title:"Kuala Lumpur's Historic KTMB Building",slug:"kuala-lumpurs-historic-ktmb-building",pub_date:"2024-05-09",hero_img_src:"./figures/malaysia.jpg",hero_img_alt:"Railway Administration Building in Kuala Lumpur taken on January 31, 2019",keywords:["Architecture","Photography","Malaysia"]},u="D:/izvw/github/astro/mad-jet/src/content/blog/post-001-ktmb-malaysia/ktmb-malaysia.md",s=void 0;function b(){return`
+# Introduction
+
+Greetings, world! I’m excited to share my first blog post with you.
+
+# About the Featured Image
+
+The image above captures an intriguing perspective of an old building, taken on January 31, 2019, using my Fujifilm XT20 camera. It showcases the Railway Administration Building in Kuala Lumpur, also known as Keretapi Tanah Melayu Berhad (KTMB). To learn more about this building, you can visit its [official website](https://www.ktmb.com.my/).
+
+For additional details about the Railway Administration Building, refer to the References section below.
+
+## References
+
+1. Railway Administration Building in Kuala Lumpur, [Malaysia Trains](https://malaysiatrains.com/railway-administration-building-in-kuala-lumpur/)
+2. Gedung Ibu Pejabat Stasiun Kereta Api Tanah Melayu, [Wikipedia](https://id.wikipedia.org/w/index.php?title=Gedung_Ibu_Pejabat_Stasiun_Kereta_Api_Tanah_Melayu&oldid=23924685)
+3. KTMB Keretapi Tanah Melayu Berhad, [Google Maps](https://maps.app.goo.gl/86AnQT5uwqYoGSux8)
+`}function f(){return e}function y(){return[{depth:1,slug:"introduction",text:"Introduction#"},{depth:1,slug:"about-the-featured-image",text:"About the Featured Image#"},{depth:2,slug:"references",text:"References#"}]}const w=i((l,d,h)=>{const{layout:p,...a}=o;return a.file=u,a.url=s,t`${n()}${r(e)}`});export{w as Content,f as compiledContent,w as default,u as file,o as frontmatter,y as getHeadings,b as rawContent,s as url};
