3af2712078dc436115471a252c6bb5c24a207fa8
arnottferels <64685389+arnottferels@users.noreply.github.com>
2024-08-02 19:07:30 +0700

    Updates

---
+++ _astro/hoisted.BLnanp8F.js
@@ -0,0 +1,1 @@
+import"./hoisted.DTaz2oWa.js";import"./index.CTbVEFV3.js";window.addEventListener("beforeprint",()=>{document.querySelectorAll("details").forEach(e=>{e instanceof HTMLDetailsElement&&(e.open=!0)})});
