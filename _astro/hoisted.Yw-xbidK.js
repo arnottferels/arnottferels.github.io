a6ae5b382f899ded16d4e82f6dca681b440673b9
arnottferels <64685389+arnottferels@users.noreply.github.com>
2024-08-04 15:05:45 +0700

    Updates 2024-08-04 15:01:09

---
+++ _astro/hoisted.Yw-xbidK.js
@@ -0,0 +1,1 @@
+import"./hoisted.CViuQBWl.js";import"./index.CTbVEFV3.js";const e={init:function(){new PagefindUI({element:"#searchContainer",debounceTimeoutMs:500,resetStyles:!1,showEmptyFilters:!1,excerptLength:10,showImages:!0,showSubResults:!0,autofocus:!0})}};e.init();document.addEventListener("astro:after-swap",e.init);
