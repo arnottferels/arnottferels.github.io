721ff64956b508d594cb6bdc968eb65c6692ad4a
arnottferels <64685389+arnottferels@users.noreply.github.com>
2024-08-09 00:41:26 +0700

    Updates 2024-08-08 00:40:06

--- _astro/hoisted.CtkOn8H1.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.CVvkhuS0.js";import"./index.CTbVEFV3.js";window.addEventListener("beforeprint",()=>{document.querySelectorAll("details").forEach(e=>{e instanceof HTMLDetailsElement&&(e.open=!0)})});
