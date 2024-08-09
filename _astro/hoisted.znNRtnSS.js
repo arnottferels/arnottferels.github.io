7aa790938a8ebd9e0d4767c4e4c54c1f09e7abb3
arnottferels <64685389+arnottferels@users.noreply.github.com>
2024-08-09 16:50:24 +0700

    Updates 2024-08-09 16:45:34

--- _astro/hoisted.znNRtnSS.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.Bd9D0kOf.js";import"./index.CTbVEFV3.js";window.addEventListener("beforeprint",()=>{document.querySelectorAll("details").forEach(e=>{e instanceof HTMLDetailsElement&&(e.open=!0)})});
